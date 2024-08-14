af184cbf35f1db5a83a8e9f86d7428d4d87db10f
Dave757 <davegulay747@gmail.com>
2024-08-14 22:21:00 +0800

    Updated Submission

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo {
+
+   public static void main(String[] args){
+ 
+      System.out.println("Movie Quote: Why so serious?");
+      System.out.println("Movie: The Dark Knight");
+      System.out.println("Character: Heath Ledger aka The Joker");
+      System.out.println("Year: 2008");
+   }
+}
