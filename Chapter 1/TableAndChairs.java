57b6d5fde037c85309ee579c06cfc0256d28a502
Dave757 <davegulay747@gmail.com>
2024-08-14 22:07:01 +0800

    Submission

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,13 @@
+public class TableAndChairs {
+
+   public static void main(String[] args) {
+   
+      System.out.println("X\t\t\t\t\t\t\t\t\tX");
+      System.out.println("X\t\t\t\t\t\t\t\t\tX");
+      System.out.println("X\t\t\tXXXXXXXXXX\t\t\tX");
+      System.out.println("XXXXX\t\tX\t\t\tX\t  XXXXX");
+      System.out.println("X   X\t\tX\t\t\tX\t  X\tX");
+      System.out.println("X   X\t\tX\t\t\tX\t  X\tX");
+
+   }
+}
---
+++ Chapter 1/Triangle.java
@@ -0,0 +1,16 @@
+public class Triangle {
+
+    public static void main(String[] args) {
+        int rows = 7; 
+        
+        for (int i = 1; i <= rows; i++) {          
+            for (int j = rows - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("T");
+            }
+            System.out.println();
+        }
+    }
+}
