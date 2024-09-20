9e90ad75507fe4bcf6c7e01b699af547530e4682
dave-gulay <davegulay747@gmail.com>
2024-09-20 19:47:34 +0800

    Methods

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,20 @@
+public class FirstAddressExcercise {
+
+      public void displayAddress() {
+      
+            System.out.println("ASUS Company");
+            System.out.println("15 Li-Te Road");
+            System.out.println("Beitou District, Taipei City");
+            System.out.println("Taiwan 112");
+            System.out.println("Product: ASUS ROG Phoney");
+   
+      }
+      
+      public static void main(String[] args) {
+            
+            FirstAddressExcercise obj = new FirstAddressExcercise();
+            obj.displayAddress();
+      
+      }
+
+}
